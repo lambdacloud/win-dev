22a7cfd92f46f61d34026b3bab46110de2ce579b
phexus <meyanhr@gmail.com>
2015-10-21 16:48:19 +0800

    chinese annotation

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,13 @@
+#win环境下不要用UTF-8编码，win版的vagrant会报错
 curl -sL https://deb.nodesource.com/setup_0.10 | sudo -E bash -
-#maven will install jdk 1.7 automatically
-sudo apt-get install -y nodejs git python-setuptools pkg-config maven 
-#install ansible by pip  
+#安装0.10.40版本的nodejs git 等工具，maven会自动安装jdk1.7
+sudo apt-get install -y nodejs git python-setuptools pkg-config maven
+#通过pip安装高版本的ansible   
 sudo easy_install -y pip
 sudo pip install -y ansible
-#install other tools through npm
+#通过npm安装其他工具
 sudo npm install -g -y gulp grunt grunt-cli
-#install scala-2.10.4
+#安装2.10.4版本的scala
 mkdir packages
 cd packages/
 curl -o scala-2.10.4.tgz http://www.scala-lang.org/files/archive/scala-2.10.4.tgz
@@ -15,6 +16,5 @@ sudo sed -i '$a\#set scala environment' /etc/profile
 sudo sed -i '$a\export SCALA_HOME=/usr/lib/scala-2.10.4' /etc/profile
 sudo sed -i '$a\export PATH=$SCALA_HOME/bin:$PATH' /etc/profile
 source /etc/profile
-#there may be some errors because of setting root password when installing mysql
-#it's suggested to install mysql in ubuntu
+#安装5.6版本的mysql，由于要设定root密码win端可能会报错，但实测安装成功，建议ubuntu下自行安装
 #sudo apt-get install -y mysql-server-5.6
